8a6d4ff20ee4b1fe9327ffbf9da18b3e50a50ea9
Shwetha K <shweetha.sweety17@gmail.com>
2025-08-16 12:20:38 +0530

    25-08-16 :: Binary Search : Shwetha K : Find first and last position of elements in sorted array, search in insert position, number of occurrence

---
+++ 34FindFirstandLastPositionofElementinSortedArray.py
@@ -0,0 +1,101 @@
+'''
+
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+ 
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+
+'''
+
+class Solution:
+      def searchRange(self, nums: list[int], target: int) -> list[int]:
+          if not nums:
+              return [-1,-1]
+          n=len(nums)
+          
+          lower=self.lowerBound(nums,target)
+          if lower==n or nums[lower]!=target:
+              return [-1,-1]
+          upper=self.upperBound(nums,target)
+          return [lower,upper-1]
+      def lowerBound(self,nums:list[int],target:int):
+          n=len(nums)
+          left,right=0,n-1 
+          index=n
+          while left<=right:
+                mid=(left+right)//2 
+                if nums[mid]>=target:
+                    index=mid 
+                    right=mid-1
+                else:
+                    left=mid+1
+          return index 
+      def upperBound(self,nums:list[int],target:int):
+          n=len(nums)
+          left,right=0,n-1 
+          index=n
+          while left<=right:
+              mid=(left+right)//2
+              if nums[mid]>target:
+                  index=mid 
+                  right=mid-1
+              else:
+                  left=mid+1
+          return index
+class Solution:
+      def searchRange(self, nums: list[int], target: int) -> list[int]:
+        result=[-1,-1]
+        n=len(nums)
+        left,right=0,n-1
+        while left<=right:
+              mid=(left+right)//2 
+              if nums[mid]==target:
+                  result[0]=mid 
+                  right=mid-1
+              elif nums[mid]<target:
+                  left=mid+1
+              else:
+                  right=mid-1
+        left,right=0,n-1
+        while left<=right:
+              mid=(left+right)//2 
+              if nums[mid]==target:
+                  result[1]=mid 
+                  left=mid+1
+              elif nums[mid]<target:
+                  left=mid+1
+              else:
+                  right=mid-1
+        return result       
+    
+class TestApp:
+      def testing_case_one(self):
+          assert Solution().searchRange([5,7,7,8,8,10],8)==[3,4]
+      def testing_case_two(self):
+          assert Solution().searchRange([5,7,7,8,8,10],6)==[-1,-1]
+      def testing_case_three(self):
+          assert Solution().searchRange([],0)==[-1,-1]
---
+++ NumberOfOcuurence.py
@@ -0,0 +1,61 @@
+'''
+Given a sorted array, arr[] and a number target, you need to find the number of occurrences of target in arr[]. 
+
+Examples :
+
+Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 2
+Output: 4
+Explanation: target = 2 occurs 4 times in the given array so the output is 4.
+Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 4
+Output: 0
+Explanation: target = 4 is not present in the given array so the output is 0.
+Input: arr[] = [8, 9, 10, 12, 12, 12], target = 12
+Output: 3
+Explanation: target = 12 occurs 3 times in the given array so the output is 3.
+Constraints:
+1 ≤ arr.size() ≤ 106
+1 ≤ arr[i] ≤ 106
+1 ≤ target ≤ 106
+
+
+'''
+
+class Solution:
+        def countFreq(self, arr:list[int], target:int):
+           n=len(arr)
+           lower=self.lowerBound(arr,target)
+           if lower==n or arr[lower]!=target:
+               return 0
+           upper=self.upperBound(arr,target)
+           return upper-lower
+        def lowerBound(self,nums:list[int],target:int)->int:
+            n=len(nums)
+            left,right=0,n-1 
+            index=n
+            while left<=right:
+                  mid=(left+right)//2 
+                  if nums[mid]>=target:
+                      index=mid 
+                      right=mid-1
+                  else:
+                      left=mid+1
+            return index  
+        def upperBound(self,nums:list[int],target:int)->int:
+            n=len(nums)
+            left,right=0,n-1
+            index=n 
+            while left<=right:
+                  mid=(left+right)//2 
+                  if nums[mid]>target:
+                      index=mid 
+                      right=mid-1
+                  else:
+                      left=mid+1
+            return index       
+class TestApp:
+      def testing_case_one(self):
+          assert Solution().countFreq([1, 1, 2, 2, 2, 2, 3],2)==4 
+      def testing_case_two(self):
+        assert Solution().countFreq([1, 1, 2, 2, 2, 2, 3],4)==0
+      def testing_case_three(self):
+          assert Solution().countFreq([1,2,2,3,3,3],3)==3
