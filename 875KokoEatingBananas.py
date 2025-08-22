569caa3885ca3a40f7db0a3b42e4420899bc2348
Shwetha K <shweetha.sweety17@gmail.com>
2025-08-22 10:47:15 +0530

    25-08-22 :: Binary Search : Shwetha K : Capacity to ship packages within d days

--- 875KokoEatingBananas.py
+++ 875KokoEatingBananas.py
@@ -1,10 +1,14 @@
 '''
 
-Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas.
+The guards have gone and will come back in h hours.
 
-Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some 
+pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, 
+she eats all of them instead and will not eat any more bananas during this hour.
 
-Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+Koko likes to eat slowly but still wants to finish eating all the bananas before the 
+guards return.
 
 Return the minimum integer k such that she can eat all the bananas within h hours.
 
@@ -35,7 +39,7 @@ import math
 
 class Solution:
       def minEatingSpeed(self, piles: list[int], h: int) -> int:
-          bananas=10**9
+          bananas=0
           n=max(piles)
           previous_hour=n
           for i in range(1,n+1):
