fea6c4a2420dbd08b9f327b0bc6d7becbe263d27
Shwetha K <shweetha.sweety17@gmail.com>
2025-09-10 13:20:13 +0530

    25-09-10 :: Binary Search : Shwetha K : Find first and last position of element or finding the range of the target element using template 3 (Binary Search)

--- 34FindFirstandLastPositionofElementinSortedArray.py
+++ 34FindFirstandLastPositionofElementinSortedArray.py
@@ -92,7 +92,53 @@ class Solution:
               else:
                   right=mid-1
         return result       
-    
+class Solution:
+        def searchRange(self, nums: list[int], target: int) -> list[int]:
+            n=len(nums)
+            if not n:
+                return [-1,-1]
+            lower=self.lowerBound(nums,target,n)
+            if lower==n or nums[lower]!=target:
+                return [-1,-1]
+            upper=self.upperBound(nums,target,n)
+            return [lower,upper]
+        def lowerBound(self,nums:list[int],target:int,n:int)->int:
+            left,right,index=0,n-1,n 
+            while left+1<right:
+                  mid=(left+right)//2 
+                  if nums[mid]==target:
+                      index=mid 
+                      right=mid 
+                  elif nums[mid]<target:
+                       left=mid 
+                  else:
+                      right=mid 
+            
+            if left<n and nums[left]==target:
+                index=min(left,index)
+            if right<n and nums[right]==target:
+                index=min(right,index) 
+            return index 
+        def upperBound(self,nums:list[int],target:int,n:int)->int:
+            index,left,right=n,0,n-1
+            while left+1 <right:
+                  mid=(left+right)//2 
+                  if target==nums[mid]:
+                     index=mid 
+                     left=mid 
+                  elif nums[mid]<target:
+                       left=mid 
+                  else:
+                      right=mid 
+            if right<n and nums[left]==target :
+                index=left
+            if left<n and nums[right]==target :
+                index=right
+            return index
+            
+Solution().searchRange([1],1)            
+          
+            
 class TestApp:
       def testing_case_one(self):
           assert Solution().searchRange([5,7,7,8,8,10],8)==[3,4]
@@ -100,3 +146,7 @@ class TestApp:
           assert Solution().searchRange([5,7,7,8,8,10],6)==[-1,-1]
       def testing_case_three(self):
           assert Solution().searchRange([],0)==[-1,-1]
+      def testing_case_four(self):
+          assert Solution().searchRange([2,2],2)==[0,1]
+      def testing_case_five(self):
+          assert Solution().searchRange([1],1)==[0,0]
